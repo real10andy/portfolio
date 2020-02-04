3ad56343a9948c17d119072d8e438a78c387bce9
real10andy <52812574+real10andy@users.noreply.github.com>
2020-02-04 13:09:54 +0900

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -110,3 +110,25 @@ $(function(){
     });
   });
 });
+
+$(function() {
+  var h = $(window).height();
+   $('#loading__wrapper').css('display','none');
+   $('#is-loading ,#loading').height(h).css('display','block');
+  });
+ 
+  $(window).load(function () {
+   $('#is-loading').delay(900).fadeOut(800);
+   $('#loading').delay(600).fadeOut(300);
+   $('#loading__wrapper').css('display', 'block');
+  });
+ 
+  $(function(){
+   setTimeout('stopload()',10000);
+   });
+ 
+   function stopload(){
+    $('#loading__wrapper').css('display','block');
+    $('#is-loading').delay(900).fadeOut(800);
+    $('#loading').delay(600).fadeOut(300);
+  }
